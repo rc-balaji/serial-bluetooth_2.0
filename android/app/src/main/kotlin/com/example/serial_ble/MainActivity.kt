9da4a69fc32e424e6bf0b2806dd8da6c26ba628e
rc-balaji <rcbalaji2003@gmail.com>
2024-03-11 14:24:41 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/serial_ble/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.serial_ble
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/MainPage.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+import './ChatPage.dart';
+import './SelectBondedDevicePage.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPage createState() => new _MainPage();
+}
+
+class _MainPage extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Bluetooth Serial'),
+      ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: ElevatedButton(
+                child: const Text('Connect to paired device to chat'),
+                onPressed: () async {
+                  final BluetoothDevice? selectedDevice =
+                  await Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return SelectBondedDevicePage(checkAvailability: false);
+                      },
+                    ),
+                  );
+
+                  if (selectedDevice != null) {
+                    print('Connect -> selected ' + selectedDevice.address);
+                    _startChat(context, selectedDevice);
+                  } else {
+                    print('Connect -> no device selected');
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _startChat(BuildContext context, BluetoothDevice server) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return ChatPage(server: server);
+        },
+      ),
+    );
+  }
+}
